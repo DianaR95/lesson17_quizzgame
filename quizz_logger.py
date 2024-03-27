d6c3d5a1fff6a34b96621d5365448034b72025fc
Diana Radulescu <radulescu.diana95@gmail.com>
2024-03-27 20:34:49 +0200

    added logging for errors

--- quizz_logger.py
+++ quizz_logger.py
@@ -3,12 +3,12 @@ import logging
 import coloredlogs
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s | %(filename)s | %(levelname)s | %line: (lineno)d | %(message)s', level=logging.DEBUG)
+# logging.basicConfig(filename="quiz_log.log", filemode="w", format=' %(asctime)s| %(filename)s | %(levelname)s | line: %(lineno)d | %(message)s', level=logging.DEBUG)
+logging.basicConfig(format=' %(asctime)s| %(filename)s | %(levelname)s | line: %(lineno)d | %(message)s', level=logging.DEBUG)
 coloredlogs.install(level='DEBUG', logger=logger)
 
 if __name__ == '__main__':
-
-    logger.debug("Aici e un debug")
-    logger.info("Aici e un infor print")
-    logger.warning("Aici e un warning")
-    logger.error("Aici e o eroare")
+    logger.debug("Aici eu un debug00")
+    logger.info("Aici e un infor print ")
+    logger.warning("Aici este un warning")
+    logger.error("aici e o eroar")
